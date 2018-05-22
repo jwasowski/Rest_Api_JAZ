710a978fa7f9b05e76c5009aff434d7a96b32242
jwasowski <jakub.wasowski@gmail.com>
2018-05-22 17:33:45 +0200

    Dodawanie produktow i komentarzy
    TODO: Doimplementowanie brakujacych metod, rozwiazanie problemu ze stosem i niekonczaca sie rekurencja

--- src/main/java/domain/Comment.java
+++ src/main/java/domain/Comment.java
@@ -1,10 +1,24 @@
 package domain;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
 public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String date;
 	private String contents;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Product product;
 
 	public int getId() {
 		return id;
@@ -36,6 +50,14 @@ public class Comment {
 
 	public void setContents(String contents) {
 		this.contents = contents;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
 	}
 
 }
--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -1,13 +1,33 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement
+@Entity
+@NamedQueries({
+@NamedQuery(name="product.all", query="Select p from Product p"),
+@NamedQuery(name="product.id", query="from Product p where p.id=:productId")})
+
 public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private double price;
 	private String category;
-	private List<Comment> commentList;
+	private List<Comment> commentList = new ArrayList<Comment>();
 
 	public int getId() {
 		return id;
@@ -40,7 +60,9 @@ public class Product {
 	public void setCategory(String category) {
 		this.category = category;
 	}
-
+	
+	//@XmlTransient
+	@OneToMany(mappedBy="product", orphanRemoval=true, fetch = FetchType.LAZY)
 	public List<Comment> getCommentList() {
 		return commentList;
 	}
